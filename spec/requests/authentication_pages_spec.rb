faee3cad62ead6eacf5a504fb5ca2f91cddde75f
Andy Plaisted <andrew.plaisted@gmail.com>
2013-06-30 22:52:12 -0700

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -60,7 +60,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       
       describe "when attempting to visit a protected page" do
-        let(:user) {FactoryGirl.create(:user) }
+        let(:user) { FactoryGirl.create(:user) }
         
         describe "when attempting to visit a protected page" do
           before do 
@@ -86,6 +86,19 @@ describe "Authentication" do
               end
             end
           end
+        end
+      end
+      
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+        
+        describe "submitting to the destory action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
         end
       end
       
